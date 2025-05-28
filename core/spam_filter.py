83d905d0dc372ac55ed9d4b70f88c394dd22e7bb
Yuztrazure <artemijsidnev04923@gmail.com>
2025-05-28 18:48:02 +0300

    Добавил в filter использование данных из data

--- core/spam_filter.py
+++ core/spam_filter.py
@@ -1,7 +1,26 @@
-SPAM_KEYWORDS = ["win"]
-SPAM_URLS = ["https://www.hse.ru"]
+import csv
+import os 
 
-#НАДО ДОДЕЛАТЬ ЭТУ ЛОГИКУ АЛЕ
+base_dir = os.path.dirname(os.path.dirname(__file__))
+file_path = os.path.join(base_dir, "data", "spam_dataset.csv")
+
+temp_spam_dict = {}
+with open(file_path, newline='', encoding='utf-8') as csvdataset:
+    reader = csv.reader(csvdataset, delimiter='\t')
+    for row in reader:
+        if len(row) == 2:
+            word = row[0].strip()
+            try:
+                value = int(row[1].strip())
+                temp_spam_dict[word] = value
+            except ValueError:
+                print("Ошибка в data-сете")
+                raise ValueError(f"Не удается преобразовать строчку {row}")
+
+SPAM_KEYWORDS = temp_spam_dict
+SPAM_URLS = {"https://www.hse.ru": 3} #in process
+
+
 def is_spam(text: str|list[str], threshold: float = 0.3) -> bool:
     if isinstance(text, list):
         text = " ".join(text)
@@ -11,10 +30,11 @@ def is_spam(text: str|list[str], threshold: float = 0.3) -> bool:
     if total_words == 0:
         return False
     
-    spam_word_count = 0
-    for keyword in SPAM_KEYWORDS + SPAM_URLS:
+    spam_values_count = 0
+    for keyword, value in {**SPAM_KEYWORDS, **SPAM_URLS}.items():
         if keyword in text_lower:
-            spam_word_count += 1
+            spam_values_count += value
 
-    spam_ratio = spam_word_count / total_words
+
+    spam_ratio = spam_values_count / total_words
     return spam_ratio >= threshold
